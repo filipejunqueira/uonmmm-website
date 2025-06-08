63a7e8174fa62347fc09c84be49a374a9f919d7b
filipejunqueira <filipelqj@gmail.com>
2025-06-08 19:32:26 -0300

    workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This tells Vite that all assets should be loaded relative to this path
+  // on the server.
+  base: "/uonmmm-website/",
+
   plugins: [react()],
-})
+});
